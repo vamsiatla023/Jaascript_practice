6ef8491082d81724f952b6323f26c04cb44e0da9
vamsiatla023 <134207974+vamsiatla023@users.noreply.github.com>
2026-02-26 16:00:31 +0000

    variables

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all Js code as a newer version
+// alert(3 + 3) we are using the node not the browser 
+console.log(3 + 3) //no need of the semicolon when the new another line is written in the new line
+console.log("vamsi atla")
+
+//number range 2 to the power 53
+
+//bigint
+
+//string => ""
+
+//boolean => true/false
+
+//null => standalone value and an object also
+
+//undefined => value is not assigned (but not null)
+
+//symbol => for the uniqueness
+
+
+
+// primitie data types
+
+// 1.object
+console.log(typeof "amsitla")
+
+console.log(typeof undefined) //undefined
+
+console.log(typeof null) // object
