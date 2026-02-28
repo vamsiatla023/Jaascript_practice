cb11cd9cf9e23677ef5cd485e3f74f9172b24b72
vamsiatla023 <134207974+vamsiatla023@users.noreply.github.com>
2026-02-28 10:11:30 +0000

    datatype_conversion

---
+++ 01_basics/datatypes_coparision.js
@@ -0,0 +1,12 @@
+// === is comparision 
+// but == is conersion 
+
+console.log(null < 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined < 0)
+console.log(undefined > 0)
+console.log(undefined == 0)
+
+console.log("2" == 2)
