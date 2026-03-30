bb0781b1af07b58f0fd4c56a1490aec8132ab50d
vamsiatla023 <134207974+vamsiatla023@users.noreply.github.com>
2026-03-30 16:25:20 +0000

    while

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+
+// While and Do while Loop
+
+let index = 0;
+while(index <= 10){
+//   console.log(`alue is ${index}`);
+  index=index+2;
+}
+
+let myarray=['spiderman','Thar','BP'];
+let arr=0;
+while(arr < myarray.length){
+//  console.log(`value is ${myarray[arr]}`);
+ arr = arr +1;
+}
+
+let score= 11;
+do {
+    console.log(`score is:${score}`);
+}while(score <= 10);
